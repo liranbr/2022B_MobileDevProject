be9c130949594e641003096d098b53de383831ea
RonenGurevich <gurevichronen@gmail.com>
2022-06-17 13:50:44 +0300

    added tab logic

--- app/src/main/java/com/example/mobiledevproject/MainActivity.java
+++ app/src/main/java/com/example/mobiledevproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mobiledevproject;
 
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -12,19 +13,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mobiledevproject.ui.main.SectionsPagerAdapter;
 //import com.example.mobiledevproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    enum modes {
+        NAVIGATION,
+        LOOKAROUND
+    }
 //    private ActivityMainBinding binding;
+    TextView MenuText;
+    Button reachLocationBTN;
+    Button onSiteBTN;
+    MaterialButtonToggleGroup toggleGroup;
+    int currentMode = modes.NAVIGATION.ordinal(); // default mode is navigation
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        findviews();
+        reachLocationBTN.setEnabled(false);
+        onSiteBTN.setEnabled(false);
 
+        reachLocationBTN.setVisibility(View.INVISIBLE);
+        onSiteBTN.setVisibility(View.INVISIBLE);
+
+        toggleGroup.addOnButtonCheckedListener((MaterialButtonToggleGroup group, int checkedId, boolean isChecked)-> {
+                if (isChecked) {
+                    if(checkedId == R.id.button1) {
+                        currentMode = modes.NAVIGATION.ordinal();
+                        MenuText.setText("Where To?");
+                    } else if(checkedId == R.id.button2) {
+                        currentMode = modes.LOOKAROUND.ordinal();
+                        MenuText.setText("Look Where?");
+                    }
+                }
+        });
 
         /* // Instantiate tabs and viewpager
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -36,5 +65,13 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
         */
+    }
+
+    void findviews()
+    {
+        MenuText = findViewById(R.id.textView2);
+        toggleGroup = findViewById(R.id.toggleButton);
+        reachLocationBTN = findViewById(R.id.reach_location);
+        onSiteBTN = findViewById(R.id.im_at_location);
     }
 }
