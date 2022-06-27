fedff43259f696591adce8560d894bf70bdd0aa3
liranbr <liranbraverman@gmail.com>
2022-06-27 20:19:53 +0300

    adding WIP Firebase authentication

--- app/src/main/java/com/example/mobiledevproject/ReportActivity.java
+++ app/src/main/java/com/example/mobiledevproject/ReportActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ReportActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
